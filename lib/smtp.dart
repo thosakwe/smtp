03b5aebc8f0bf7bf22c9df81162210990844ca52
Tobe O <thosakwe@gmail.com>
2018-02-27 23:53:04 -0500

    Date format

--- lib/smtp.dart
+++ lib/smtp.dart
@@ -3,6 +3,9 @@ library smtp;
 import 'dart:async';
 import 'dart:io';
 import 'package:async/async.dart';
+import 'package:intl/intl.dart';
 
 part 'src/protocol.dart';
 part 'src/server.dart';
+
+final DateFormat _fmt = new DateFormat('EEE, d MMM yyyy HH:mm:ss');
