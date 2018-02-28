a5327ec5fb70ba2ce18075b678d9335768433225
Tobe O <thosakwe@gmail.com>
2018-02-27 23:51:29 -0500

    Protocol base

--- lib/smtp.dart
+++ lib/smtp.dart
@@ -3,5 +3,6 @@ library smtp;
 import 'dart:async';
 import 'dart:io';
 import 'package:async/async.dart';
+
 part 'src/protocol.dart';
 part 'src/server.dart';
--- lib/src/protocol.dart
+++ lib/src/protocol.dart
@@ -1,2 +1,34 @@
 part of smtp;
 
+abstract class SmtpRequest {
+  SmtpHeaders get headers;
+  SmtpConnectionInfo connectionInfo;
+  String message;
+}
+
+abstract class SmtpHeaders {
+  DateTime get date;
+  String get from;
+  String get to;
+  String get cc;
+  String get subject;
+}
+
+abstract class SmtpConnectionInfo {
+  InternetAddress get remoteAddress;
+  int get localPort;
+  int get remotePort;
+}
+
+class _SmtpHeadersImpl implements SmtpHeaders {
+  final Map<String, String> headers;
+
+  _SmtpHeadersImpl(this.headers);
+}
+
+class _SmtpConnectionInfoImpl implements SmtpConnectionInfo {
+  final InternetAddress remoteAddress;
+  final int localPort, remotePort;
+
+  _SmtpConnectionInfoImpl(this.remoteAddress, this.localPort, this.remotePort);
+}
